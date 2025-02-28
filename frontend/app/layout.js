0c7c35ddd0e5672a90416cc24f3c0cb77e119f30
sumit <mazumdarsumit3@gmail.com>
2025-02-28 08:32:22 +0530

    add: wallet adapter

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import { PrivyProvider } from "@privy-io/react-auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const PRIVY_APP_ID = process.env.PRIVY_ID;
   return (
     <html lang="en">
       <head>
-        
-
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com"  />
       <link href="https://fonts.googleapis.com/css2?family=Anton&family=Doto:wght@100..900&family=Liter&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Playwrite+ES+Deco+Guides&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&family=Sigmar&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&family=Style+Script&display=swap" rel="stylesheet" />
@@ -42,7 +42,23 @@ export default function RootLayout({ children }) {
       <body
         className={`Poppins`}
       >
+        <PrivyProvider
+      appId={PRIVY_APP_ID}
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+        //   logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
         {children}
+        </PrivyProvider>
       </body>
     </html>
   );
